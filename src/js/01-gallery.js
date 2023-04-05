a3a31153c8367bfb6271b0614e5e2a2e9636db6e
yashbon <104573777+yashbon@users.noreply.github.com>
2023-04-06 00:46:15 +0300

    Completed Task - 1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -2,4 +2,28 @@
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log('hello!\n', galleryItems);
+// Описаний в документації
+import SimpleLightbox from "simplelightbox";
+// Додатковий імпорт стилів
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+console.log(galleryItems);
+
+
+const galleryList = document.querySelector('.gallery');
+
+const markup = galleryItems.map(
+    ({ description, preview, original }) =>
+        `<li class="gallery__item">
+            <a class="gallery__link" href="${original}">
+                <img class="gallery__image" src="${preview}" alt="${description}" />
+            </a>
+        </li>`
+).join('');
+galleryList.insertAdjacentHTML('afterbegin', markup);
+
+let lightbox = new SimpleLightbox('.gallery__item .gallery__link', {
+    /* options */
+    captionsData: 'alt',
+    captionDelay: 250,
+});
