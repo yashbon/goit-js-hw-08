6834d399c8984f9cce51fe815bf9d109d3905cbc
yashbon <104573777+yashbon@users.noreply.github.com>
2023-04-10 02:06:45 +0300

    Fixed Task - 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -10,4 +10,4 @@ function onPlay(data) {
     localStorage.setItem('videoplayer-current-time', data.seconds);
 }
 
-player.setCurrentTime(localStorage.getItem('videoplayer-current-time') || 0);
+player.setCurrentTime(JSON.parse(localStorage.getItem('videoplayer-current-time')) || 0);
