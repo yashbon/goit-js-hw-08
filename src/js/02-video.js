3b4bb7980d3efe806b07c2eb4f70d5fe1a4814b7
yashbon <104573777+yashbon@users.noreply.github.com>
2023-04-09 23:39:00 +0300

    Completed Task - 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,13 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe')
+const player = new Player(iframe);
+
+player.on('timeupdate', throttle(onPlay, 1000));
+
+function onPlay(data) {
+    localStorage.setItem('videoplayer-current-time', data.seconds);
+}
+
+player.setCurrentTime(localStorage.getItem('videoplayer-current-time') || 0);
