9c91febf31ddf9091769f243728e1b3ac7ae990b
yashbon <104573777+yashbon@users.noreply.github.com>
2023-04-10 17:02:51 +0300

    Updated 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -4,10 +4,10 @@ import throttle from 'lodash.throttle';
 const iframe = document.querySelector('iframe')
 const player = new Player(iframe);
 const LOCALSTOR_KEY = 'videoplayer-current-time';
+
 player.on('timeupdate', throttle(onPlay, 1000));
+player.setCurrentTime(localStorage.getItem(LOCALSTOR_KEY) || 0);
 
 function onPlay(data) {
     localStorage.setItem(LOCALSTOR_KEY, data.seconds);
 }
-
-player.setCurrentTime(localStorage.getItem(LOCALSTOR_KEY) || 0);
