8df3aadc198724447d122ec67715719fddebc742
yashbon <104573777+yashbon@users.noreply.github.com>
2023-04-10 17:03:47 +0300

    Completed Task - 3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,29 @@
+import throttle from 'lodash.throttle';
 
+const form = document.querySelector('.feedback-form')
+const LOCALSTOR_KEY = 'feedback-form-state';
+const { email, message } = form.elements;
+
+let dataForm = JSON.parse(localStorage.getItem('LOCALSTOR_KEY')) || {};
+if (dataForm) {
+    email.value = dataForm.email || '';
+    message.value = dataForm.message || '';
+}
+
+form.addEventListener('input', throttle(onFormInput), 500);
+form.addEventListener('submit', onFormSubmit);
+
+function onFormInput(event) {
+    const { elements: { email, message } } = event.currentTarget;
+    localStorage.setItem('LOCALSTOR_KEY', JSON.stringify({ email: email.value, message: message.value }));
+}
+
+function onFormSubmit(event) {
+    event.preventDefault();
+
+    const { elements: { email, message } } = event.currentTarget;
+    console.log(email.value, message.value);
+
+    localStorage.removeItem('LOCALSTOR_KEY');
+    event.currentTarget.reset();
+}
